88deff80699b31a9b774486a3eba5d81cbd80ca2
chauhankevin982@gmail.com <chauhankevin982@gmail.com>
2023-03-12 23:44:57 +0530

    compound interest

---
+++ COMPOUND.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+	float c,p,r,t,a;
+	clrscr();
+
+	printf("Enter the principle amount : ");
+	scanf("%f",&p);
+	printf("Enter the annual rate of interest : ");
+	scanf("%f",&r);
+	printf("Enter the period in years : ");
+	scanf("%f",&t);
+	a = (1+(r/100));
+	a = pow(a,t)*p;
+	c = a-p;
+	printf("Compound interest of %.0f at %.2f rate of interest for period %.0f is : %.2f",p,r,t,c);
+
+	getch();
+}
